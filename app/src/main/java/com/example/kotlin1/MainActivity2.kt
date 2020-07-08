7f5691e99d3d5be5629152f85547022cbf8d0eca
Alex <alexydenkov99@gmail.com>
2020-07-09 00:24:57 +0700

    Теперь при нажатии на бур он тресятся 5 циклов и замирает

--- app/src/main/java/com/example/kotlin1/MainActivity2.kt
+++ app/src/main/java/com/example/kotlin1/MainActivity2.kt
@@ -1,12 +1,46 @@
 package com.example.kotlin1
 
+import android.content.Intent
+import android.graphics.drawable.Animatable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val Bur: ImageView = findViewById(R.id.bur)
+
+        val imageViews: MutableList<ImageView> =
+                ArrayList()
+
+        imageViews.add(findViewById<View>(R.id.bur) as ImageView)
+
+        //animateImageViews(imageViews)
+        var a = true
+        Bur.setOnClickListener {
+                //если a равно true, то запустить анимацию
+                animateImageViews(imageViews)
+
+
+        }
     }
 
+    private fun animateImageViews(imageViews: List<ImageView>) {
+        for (imageView in imageViews) {
+            animateImageView(imageView)
+        }
+    }
+
+    private fun animateImageView(imageView: ImageView) {
+        val drawable = imageView.drawable
+        if (drawable is Animatable) {
+            val animatable = drawable as Animatable
+            animatable.start()
+        }
+    }
 }
+
